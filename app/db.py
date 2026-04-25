9024934890c0b47ce47e1c73d8dd73b5aa63412d
ang3lCerda <cerda086@gmail.com>
2026-04-24 23:07:35 -0500

    stuff needed to deploy the api

--- app/db.py
+++ app/db.py
@@ -1,5 +1,8 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 client = AsyncIOMotorClient(os.getenv("MONGODB_URI"))
 db = client.vct_fantasy  
